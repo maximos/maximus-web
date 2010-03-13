0ca748aba94ea8b01661d35d23ecc711f0d9b42a
Htbaa <c.kras@pcc-online.net>
2010-03-13 10:44:36 +0100

    Dropping SQL for modules. Will be used for users and authentication though.

--- Maximus/lib/Maximus/Schema.pm
+++ Maximus/lib/Maximus/Schema.pm
@@ -8,17 +8,13 @@ use warnings;
 
 use base 'DBIx::Class::Schema';
 
-__PACKAGE__->load_namespaces(
-    result_namespace => 'Result',
-);
+__PACKAGE__->load_namespaces;
 
 
-# Created by DBIx::Class::Schema::Loader v0.05001 @ 2010-03-06 20:29:52
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:B8JYrHL6+EE9zZQoo6tdNg
-
+# Created by DBIx::Class::Schema::Loader v0.05001 @ 2010-03-13 10:41:04
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:IuAzXlr8yFSFXtMtgWs01g
 
 __PACKAGE__->load_components(qw/Schema::Versioned/);
 __PACKAGE__->upgrade_directory('sql/');
 our $VERSION = '0.001';
-
 1;
