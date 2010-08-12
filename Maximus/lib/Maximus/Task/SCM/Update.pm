0ea31c3273cc16989ec6ed74f5e876251f8eb962
Htbaa <c.kras@pcc-online.net>
2010-08-12 17:57:58 +0200

    Removed maximus_module-update.pl and transfered functionality to Maximus::Task::SCM::Update

---
+++ Maximus/lib/Maximus/Task/SCM/Update.pm
@@ -0,0 +1,142 @@
+package Maximus::Task::SCM::Update;
+use Moose;
+use Digest::SHA qw(sha1_hex);
+use File::Spec;
+use File::Path qw(make_path);
+use Path::Class;
+use Maximus::Class::Module;
+use Maximus::Class::Module::Source::SCM::Git;
+use Maximus::Class::Module::Source::SCM::Subversion;
+
+with 'Maximus::Role::Task';
+
+=head1 NAME
+
+Maximus::Task::SCM::Update - Update SCM configurations
+
+=head1 SYNOPSIS
+
+	use Maximus::Task::SCM::Update;
+	$task->run();
+
+=head1 DESCRIPTION
+
+Update module database.
+
+=head1 METHODS
+
+=head2 run
+
+Run task
+=cut
+sub run {
+	my($self, $scm_id) = @_;
+	my $rs = $self->schema->resultset('Scm');
+	# Fetch all or search for given SCM
+	foreach my $scm( $scm_id ? $rs->search({id => $scm_id}) : $rs->all ) {
+		my $source = $self->get_source($scm);
+		my $latest_rev = $source->get_latest_revision;
+		if(!$scm->revision || !$latest_rev || $scm->revision ne $latest_rev) {
+			foreach my $module( $scm->modules ) {
+				my %versions = $source->get_versions;
+				# Skip existing versions
+				delete $versions{$_->version} for($module->module_versions->all);
+				# But always retrieve the latest dev version
+				$versions{'dev'} = 1;
+				foreach my $version(keys %versions) {
+					# New Source object
+					my $source = $self->get_source($scm);
+					if($scm->software eq 'svn' && $module->scm_settings) {
+						if(exists $module->scm_settings->{trunk}) {
+							$source->trunk( $module->scm_settings->{trunk} );
+						}
+						if(exists $module->scm_settings->{tags}) {
+							$source->tags( $module->scm_settings->{tags} );
+						}
+						if(exists $module->scm_settings->{tags_filter}) {
+							$source->tags_filter( $module->scm_settings->{tags_filter} );
+						}
+					}
+					
+					$source->version($version);
+					eval {
+						my $mod = Maximus::Class::Module->new(
+							modscope => $module->modscope->name,
+							mod => $module->name,
+							desc => $module->desc,
+							source => $source,
+							schema => $self->schema,
+						);
+						$mod->save( $module->modscope->user_id );
+					};
+					warn $@ if $@;
+				}
+			}
+		}
+		
+		$scm->update({
+			revision => $latest_rev
+		});
+	}
+	1;
+}
+
+=head2
+
+Retrieve a C<Maximus::Class::Module::Source::SCM> type object
+=cut
+sub get_source {
+	my($self, $scm) = @_;
+	my $source;
+	if($scm->software eq 'git') {
+		my $local_repo = Path::Class::Dir->new(
+			File::Spec->tmpdir(),
+			$self->cfg->{name},
+			'repositories',
+			sha1_hex($scm->repo_url)
+		);
+		make_path($local_repo->absolute->stringify);
+
+		$source = Maximus::Class::Module::Source::SCM::Git->new(
+			repository => $scm->repo_url,
+			local_repository => $local_repo->absolute->stringify,
+		);
+	}
+	elsif($scm->software eq 'svn') {
+		$source = Maximus::Class::Module::Source::SCM::Subversion->new(
+			repository => $scm->repo_url,
+		);
+	}
+	
+	return $source;
+}
+
+=head1 AUTHOR
+
+Christiaan Kras
+
+=head1 LICENSE
+
+Copyright (c) 2010 Christiaan Kras
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+=cut
+
+1;
