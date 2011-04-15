7328ecab91e5ca0a2350045a99dec312e3b5539c
Htbaa <c.kras@pcc-online.net>
2011-04-16 00:27:53 +0200

    retab

--- Maximus/root/static/js/template.js
+++ Maximus/root/static/js/template.js
@@ -12,8 +12,8 @@ window.addEvent('domready', function() {
 });
 
 var template = {
-	doForms: function() {
-		$$('form.jsForm').each(function(form) {
+    doForms: function() {
+        $$('form.jsForm').each(function(form) {
             // Prepare form inputs to hold error messages for invalid input
             form.getElements('[type=text], [type=password], textarea').each(function(el) {
                 var id = el.get('id') + '_error_msg'
@@ -26,93 +26,93 @@ var template = {
             });
 
             new Form.Validator.Inline(form);
-		});
-		
-		//theme inputs (only in ff2+ ie7+ chrome safari3 etc)
-		if ((Browser.ie && Browser.version >= 6) || Browser.chrome || Browser.safari || Browser.opera || (Browser.gecko && Browser.version >= 18)) { 
-			$$('input[type=text],input[type=password],textarea,select').each(function(element) {
-				//contain the input
-				var container = new Element('div',{'style':'display:inline-block;'}).wraps(element);
-				
-				//theme the input
-				template.roundSingleElement('Input',container);
-				
-				//add focus events
-				element.addEvents({
-					'focus': function() {
-						container.addClass('skinInputFocusedBox');
-					},
-					'blur': function() {
-						container.removeClass('skinInputFocusedBox');
-					}
-				});
-			});
-		}
-	},
-		
-	doRoundedBoxes: function(type) {
-		//add rounded theme to boxes
-		type = type.capitalize();
-		
-		$$('.js'+type+'Box').each(function(element) {
-			template.roundSingleElement(type,element);
-		})
-	},
-	
-	doCloseableBoxes: function() {
-		//add close button to boxes
-		$$('.jsCloseableBox').each(function(element) {
-			element.addClass('skinCloseIconOff').addEvents({
-				'mouseenter':function() {
-					element.removeClass('skinCloseIconOff').addClass('skinCloseIconOn');
-				},
-				'mouseleave':function() {
-					element.removeClass('skinCloseIconOn').addClass('skinCloseIconOff');
-				},
-				'click': function() {
-				    var fx = new Fx.Tween(element);
-					
-					if (Browser.ie) {
-						//internet explorer
-						if (Browser.version <= 4) {
-							//ie6
-							element.destroy();
-						} else {
-							//newer
-							element.setStyle('overflow','hidden');
-						    fx.start('height', '0').chain(function() {
-						        element.destroy();
-						    });
-						}
-					} else {
-						//others (good browsers)
-					    fx.start('opacity', '0').chain(function() {
-					        this.start('height', '0')
-					    }).chain(function() {
-					        element.destroy();
-					    });
-	    			}
-				}
-			})
-		});
-	},
-	
-	roundSingleElement: function(type,element) {
-		element.removeClass('js'+type+'Box');
-		var row1 = new Element('div', {'class': 'skin'+type+'Row1'}).adopt(
-			new Element('div', {'class': 'skin'+type+'TopLeft'}),
-			new Element('div', {'class': 'skin'+type+'TopRight'}),
-			new Element('div', {'class': 'skin'+type+'Top'})
-		);
-		var row2 = new Element('div', {'class': 'skin'+type+'Row2'}).adopt(
-			new Element('div',{'class':'skin'+type+'Content'}).adopt(element.childNodes)
-		);
-		var row3 = Element('div', {'class': 'skin'+type+'Row3'}).adopt(
-			new Element('div', {'class': 'skin'+type+'BottomLeft'}),
-			new Element('div', {'class': 'skin'+type+'BottomRight'}),
-			new Element('div', {'class': 'skin'+type+'Bottom'})
-		);
-		element.empty().addClass('skin'+type+'Box').adopt(row1,row2,row3);
-	}
+        });
+        
+        //theme inputs (only in ff2+ ie7+ chrome safari3 etc)
+        if ((Browser.ie && Browser.version >= 6) || Browser.chrome || Browser.safari || Browser.opera || (Browser.gecko && Browser.version >= 18)) { 
+            $$('input[type=text],input[type=password],textarea,select').each(function(element) {
+                //contain the input
+                var container = new Element('div',{'style':'display:inline-block;'}).wraps(element);
+                
+                //theme the input
+                template.roundSingleElement('Input',container);
+                
+                //add focus events
+                element.addEvents({
+                    'focus': function() {
+                        container.addClass('skinInputFocusedBox');
+                    },
+                    'blur': function() {
+                        container.removeClass('skinInputFocusedBox');
+                    }
+                });
+            });
+        }
+    },
+        
+    doRoundedBoxes: function(type) {
+        //add rounded theme to boxes
+        type = type.capitalize();
+        
+        $$('.js'+type+'Box').each(function(element) {
+            template.roundSingleElement(type,element);
+        })
+    },
+    
+    doCloseableBoxes: function() {
+        //add close button to boxes
+        $$('.jsCloseableBox').each(function(element) {
+            element.addClass('skinCloseIconOff').addEvents({
+                'mouseenter':function() {
+                    element.removeClass('skinCloseIconOff').addClass('skinCloseIconOn');
+                },
+                'mouseleave':function() {
+                    element.removeClass('skinCloseIconOn').addClass('skinCloseIconOff');
+                },
+                'click': function() {
+                    var fx = new Fx.Tween(element);
+                    
+                    if (Browser.ie) {
+                        //internet explorer
+                        if (Browser.version <= 4) {
+                            //ie6
+                            element.destroy();
+                        } else {
+                            //newer
+                            element.setStyle('overflow','hidden');
+                            fx.start('height', '0').chain(function() {
+                                element.destroy();
+                            });
+                        }
+                    } else {
+                        //others (good browsers)
+                        fx.start('opacity', '0').chain(function() {
+                            this.start('height', '0')
+                        }).chain(function() {
+                            element.destroy();
+                        });
+                    }
+                }
+            })
+        });
+    },
+    
+    roundSingleElement: function(type,element) {
+        element.removeClass('js'+type+'Box');
+        var row1 = new Element('div', {'class': 'skin'+type+'Row1'}).adopt(
+            new Element('div', {'class': 'skin'+type+'TopLeft'}),
+            new Element('div', {'class': 'skin'+type+'TopRight'}),
+            new Element('div', {'class': 'skin'+type+'Top'})
+        );
+        var row2 = new Element('div', {'class': 'skin'+type+'Row2'}).adopt(
+            new Element('div',{'class':'skin'+type+'Content'}).adopt(element.childNodes)
+        );
+        var row3 = Element('div', {'class': 'skin'+type+'Row3'}).adopt(
+            new Element('div', {'class': 'skin'+type+'BottomLeft'}),
+            new Element('div', {'class': 'skin'+type+'BottomRight'}),
+            new Element('div', {'class': 'skin'+type+'Bottom'})
+        );
+        element.empty().addClass('skin'+type+'Box').adopt(row1,row2,row3);
+    }
 };
 
