b1057d5c9b2d446dc0a425f6c3f89c9b86057a98
Htbaa <c.kras@pcc-online.net>
2011-04-16 16:39:33 +0200

    Premature returning

--- Maximus/lib/Maximus/Schema/Result/Module.pm
+++ Maximus/lib/Maximus/Schema/Result/Module.pm
@@ -151,8 +151,6 @@ __PACKAGE__->inflate_column('scm_settings', {
 	inflate => sub { JSON::Any->jsonToObj(shift || '{}' ) },
 	deflate => sub { JSON::Any->objToJson(shift || {} ) },
 });
-1;
-
 
 # You can replace this text with custom content, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
