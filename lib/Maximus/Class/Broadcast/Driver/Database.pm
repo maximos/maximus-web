18c7f4e5f52bd94eabce83d04dbf56023151be36
Htbaa <c.kras@pcc-online.net>
2011-04-24 10:52:25 +0200

    Moved everything from Maximus/ to root

---
+++ lib/Maximus/Class/Broadcast/Driver/Database.pm
@@ -0,0 +1,74 @@
+package Maximus::Class::Broadcast::Driver::Database;
+use Moose;
+use namespace::autoclean;
+with 'Maximus::Role::Broadcast::Driver';
+
+=head1 NAME
+
+Maximus::Class::Broadcast::Driver::Database - Database driver
+
+=head1 SYNOPSIS
+
+	use Maximus::Class::Broadcast::Driver::Database;
+	my $driver = Maximus::Class::Broadcast::Driver::Database->new;
+	my $msg = Maximus::Class::Broadcast::Message->new(text => 'Hello world!');
+	$driver->say($msg);
+
+=head1 DESCRIPTION
+
+This driver hooks up a L<DBIx::Class::Core> database table to the Broadcast
+system of Maximus.
+
+=head1 ATTRIBUTES
+
+=head2 counter
+
+Contains the number of times C<say> has been called.
+=cut
+has 'counter' => (
+	isa => 'Int',
+	is => 'rw',
+	default => 0,
+);
+
+=head1 METHODS
+
+=head2 say(L<Maximus::Class::Broadcast::Message> $msg)
+
+Ignores the message but adds 1 to the C<counter> attribute.
+=cut
+sub say {
+	my($self, $msg) = @_;
+	$self->counter( $self->counter + 1);
+}
+
+=head1 AUTHOR
+
+Christiaan Kras
+
+=head1 LICENSE
+
+Copyright (c) 2010 Christiaan Kras
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+1;
