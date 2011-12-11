022e52990c9398795b0a3afeeaf4da24bd9144d9
Christiaan Kras <c.kras@pcc-online.net>
2011-12-11 11:27:23 +0100

    Prevent form from saving empty module description as NULL
    refs gh-103

--- lib/Maximus/Role/Form/Module.pm
+++ lib/Maximus/Role/Form/Module.pm
@@ -20,11 +20,12 @@ has_field 'name' => (
 );
 
 has_field 'desc' => (
-    type      => 'Text',
-    label     => 'Description',
-    minLength => 0,
-    maxlength => 255,
-    css_class => 'minLength:0 maxLength:255',
+    type         => 'Text',
+    label        => 'Description',
+    not_nullable => 1,
+    minLength    => 0,
+    maxlength    => 255,
+    css_class    => 'minLength:0 maxLength:255',
 );
 
 no HTML::FormHandler::Moose::Role;
