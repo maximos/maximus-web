e983f436a60ae0ffea34c791fa2d6d8d3b6863d1
Christiaan Kras <c.kras@pcc-online.net>
2012-09-16 14:16:08 +0200

    Updated JavaScript files to contain compiled CoffeeScript

--- root/static/js/template.js
+++ root/static/js/template.js
@@ -1,114 +1,118 @@
-var template = {
-    doForms: function() {
-        $$('form.jsForm').each(function(form) {
-            // Prepare form inputs to hold error messages for invalid input
-            form.getElements('[type=text], [type=password], textarea, select').each(function(el) {
-                var id = el.get('id') + '_error_msg'
-                var dd_error = new Element('dd', {
-                    id: id,
-                    'class': 'error'
-                });
-                el.set('class', el.get('class') + " msgPos:'" + id + "'");
-                dd_error.inject(el.getParent('dd'), 'after');
-            });
-
-            new Form.Validator.Inline(form, {
-                errorPrefix: ''
-            });
-        });
-        
-        //theme inputs (only in ff2+ ie7+ chrome safari3 etc)
-        if ((Browser.ie && parseFloat(Browser.version) >= 6) || Browser.chrome || Browser.safari || Browser.opera || (Browser.firefox && parseFloat(Browser.version) >= 2)) { 
-            $$('input[type=text],input[type=password],textarea,select').each(function(element) {
-                //contain the input
-                var container = new Element('div',{'style':'display:inline-block;'}).wraps(element);
-                
-                //theme the input
-                template.roundSingleElement('Input',container);
-                
-                //add focus events
-                element.addEvents({
-                    'focus': function() {
-                        container.addClass('skinInputFocusedBox');
-                    },
-                    'blur': function() {
-                        container.removeClass('skinInputFocusedBox');
-                    }
-                });
-            });
-        }
-    },
-        
-    doRoundedBoxes: function(type) {
-        //add rounded theme to boxes
-        type = type.capitalize();
-        
-        $$('.js'+type+'Box').each(function(element) {
-            template.roundSingleElement(type,element);
-        })
-    },
-    
-    doCloseableBoxes: function() {
-        //add close button to boxes
-        $$('.jsCloseableBox').each(function(element) {
-            element.addClass('skinCloseIconOff').addEvents({
-                'mouseenter':function() {
-                    element.removeClass('skinCloseIconOff').addClass('skinCloseIconOn');
-                },
-                'mouseleave':function() {
-                    element.removeClass('skinCloseIconOn').addClass('skinCloseIconOff');
-                },
-                'click': function() {
-                    var fx = new Fx.Tween(element);
-                    
-                    if (Browser.ie) {
-                        //internet explorer
-                        element.setStyle('overflow','hidden');
-                        fx.start('height', '0').chain(function() {
-                            element.destroy();
-                        });
-                    } else {
-                        //others (good browsers)
-                        fx.start('opacity', '0').chain(function() {
-                            this.start('height', '0')
-                        }).chain(function() {
-                            element.destroy();
-                        });
-                    }
-                }
-            })
-        });
-    },
-    
-    roundSingleElement: function(type,element) {
-        element.removeClass('js'+type+'Box');
-        var row1 = new Element('div', {'class': 'skin'+type+'Row1'}).adopt(
-            new Element('div', {'class': 'skin'+type+'TopLeft'}),
-            new Element('div', {'class': 'skin'+type+'TopRight'}),
-            new Element('div', {'class': 'skin'+type+'Top'})
-        );
-        var row2 = new Element('div', {'class': 'skin'+type+'Row2'}).adopt(
-            new Element('div',{'class':'skin'+type+'Content'}).adopt(element.childNodes)
-        );
-        var row3 = Element('div', {'class': 'skin'+type+'Row3'}).adopt(
-            new Element('div', {'class': 'skin'+type+'BottomLeft'}),
-            new Element('div', {'class': 'skin'+type+'BottomRight'}),
-            new Element('div', {'class': 'skin'+type+'Bottom'})
-        );
-        element.empty().addClass('skin'+type+'Box').adopt(row1,row2,row3);
-    }
-};
-
 (function() {
-    //setup rounded rectangles
-    template.doRoundedBoxes('rounded');
-    template.doRoundedBoxes('error');
-    template.doRoundedBoxes('success');
-    
-    //setup closeable boxes
-    template.doCloseableBoxes();
-    
-    //setup forms
-    template.doForms();
-})();
+  var template;
 
+  template = {
+    doForms: function() {
+      $$('form.jsForm').each(function(form) {
+        form.getElements('[type=text], [type=password], textarea, select').each(function(el) {
+          var dd, dd_error, id;
+          id = el.get('id') + '_error_msg';
+          dd_error = new Element('dd', {
+            id: id,
+            "class": 'error'
+          });
+          el.set('class', el.get('class') + " msgPos:'" + id + "'");
+          dd = el.getParent('dd');
+          return dd_error.inject(dd, 'after');
+        });
+        return new Form.Validator.Inline(form, {
+          errorPrefix: ''
+        });
+      });
+      if ((Browser.ie && parseFloat(Browser.version) >= 6) || Browser.chrome || Browser.safari || Browser.opera || (Browser.firefox && parseFloat(Browser.version) >= 2)) {
+        return $$('input[type=text],input[type=password],textarea,select').each(function(element) {
+          var container;
+          container = new Element('div', {
+            style: 'display:inline-block'
+          });
+          container = container.wraps(element);
+          template.roundSingleElement('Input', container);
+          return element.addEvents({
+            focus: function() {
+              return container.addClass('skinInputFocusedBox');
+            },
+            blur: function() {
+              return container.removeClass('skinInputFocusedBox');
+            }
+          });
+        });
+      }
+    },
+    doRoundedBoxes: function(type) {
+      type = type.capitalize();
+      return $$('.js' + type + 'Box').each(function(element) {
+        return template.roundSingleElement(type, element);
+      });
+    },
+    doCloseableBoxes: function() {
+      return $$('.jsCloseableBox').each(function(element) {
+        return element.addClass('skinCloseIconOff').addEvents({
+          mouseenter: function() {
+            return element.removeClass('skinCloseIconOff').addClass('skinCloseIconOn');
+          },
+          mouseleave: function() {
+            return element.removeClass('skinCloseIconOn').addClass('skinCloseIconOff');
+          },
+          click: function() {
+            var fx;
+            fx = new Fx.Tween(element);
+            if (Browser.ie) {
+              element.setStyle('overflow', 'hidden');
+              return fx.start('height', 0).chain(function() {
+                return element.destroy();
+              });
+            } else {
+              return fx.start('opacity', 0).chain(function() {
+                return this.start('height', 0);
+              }).chain(function() {
+                return element.destroy();
+              });
+            }
+          }
+        });
+      });
+    },
+    roundSingleElement: function(type, element) {
+      var row1, row2, row3;
+      element.removeClass('js' + type + 'Box');
+      row1 = new Element('div', {
+        "class": 'skin' + type + 'Row1'
+      });
+      row1.adopt(new Element('div', {
+        "class": 'skin' + type + 'TopLeft'
+      }), new Element('div', {
+        "class": 'skin' + type + 'TopRight'
+      }), new Element('div', {
+        "class": 'skin' + type + 'Top'
+      }));
+      row2 = new Element('div', {
+        "class": 'skin' + type + 'Row2'
+      });
+      row2.adopt(new Element('div', {
+        "class": 'skin' + type + 'Content'
+      }).adopt(element.childNodes));
+      row3 = new Element('div', {
+        "class": 'skin' + type + 'Row3'
+      });
+      row3.adopt(new Element('div', {
+        "class": 'skin' + type + 'BottomLeft'
+      }), new Element('div', {
+        "class": 'skin' + type + 'BottomRight'
+      }), new Element('div', {
+        "class": 'skin' + type + 'Bottom'
+      }));
+      return element.empty().addClass('skin' + type + 'Box').adopt(row1, row2, row3);
+    }
+  };
+
+  template.doRoundedBoxes('rounded');
+
+  template.doRoundedBoxes('error');
+
+  template.doRoundedBoxes('success');
+
+  template.doCloseableBoxes();
+
+  template.doForms();
+
+}).call(this);
