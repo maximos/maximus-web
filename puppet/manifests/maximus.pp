32adefddd59ac76fe71bbcc5e56af1f566c6065c
Christiaan Kras <c.kras@pcc-online.net>
2013-08-02 19:44:09 +0200

    Puppet fix

--- puppet/manifests/maximus.pp
+++ puppet/manifests/maximus.pp
@@ -1,5 +1,18 @@
 Exec { path => "/usr/local/bin/:/bin/:/usr/bin/:/sbin:/usr/sbin/" }
 
+class params {
+    $dbname = 'maximus'
+    $dbuser = 'maximus'
+    $dbpass = 'demo'
+
+    $filepath = '/vagrant/puppet/modules'
+
+    $upload_transport = 'ftp'
+    #$upload_transport = 'filesystem'
+
+    $recaptcha = false
+}
+
 node default {
     include params
     include maximus
